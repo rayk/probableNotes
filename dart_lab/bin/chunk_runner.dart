d2d220c0fc2bdcacf4e623c2b644c6f255cf6c9f
Ray King <taipan.king@gmail.com>
2020-12-25 13:32:03 +1100

    chore(chunkrunner): Adds import of dart_lab to the chunk that is run.

--- dart_lab/bin/chunk_runner.dart
+++ dart_lab/bin/chunk_runner.dart
@@ -1,8 +1,8 @@
-import 'dart:async';
 import 'dart:isolate';
 
 Uri _remoteCode(String chunk) => Uri.dataFromString('''
    import "dart:isolate";
+   import "package:dart_lab/dart_lab.dart";
   
   void main(_, SendPort port) {
     print('Executing Code Chunk')
@@ -10,6 +10,7 @@ Uri _remoteCode(String chunk) => Uri.dataFromString('''
   }
   ''', mimeType: 'application/dart');
 
+/// Executes a chuck of code without maintaining any state.
 ReceivePort chunkRunner(String codeChunk) {
   final port = ReceivePort();
   Isolate.spawnUri(_remoteCode(codeChunk), [], port.sendPort);
