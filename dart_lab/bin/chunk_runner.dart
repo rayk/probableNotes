8fa941bb846d4360d858b735e856d72f6d28ac1c
Ray King <taipan.king@gmail.com>
2020-12-28 10:52:30 +1100

    docs(chunkrunner): Adds ability to run a single line chunk

--- dart_lab/bin/chunk_runner.dart
+++ dart_lab/bin/chunk_runner.dart
@@ -2,7 +2,6 @@ import 'dart:isolate';
 
 Uri _remoteCode(String chunk) => Uri.dataFromString('''
    import "dart:isolate";
-   import "package:dart_lab/dart_lab.dart";
   
   void main(_, SendPort port) {
     print('Executing Code Chunk')
@@ -10,7 +9,6 @@ Uri _remoteCode(String chunk) => Uri.dataFromString('''
   }
   ''', mimeType: 'application/dart');
 
-/// Executes a chuck of code without maintaining any state.
 ReceivePort chunkRunner(String codeChunk) {
   final port = ReceivePort();
   Isolate.spawnUri(_remoteCode(codeChunk), [], port.sendPort);
